ec0fe23795f4cfc7c8c26f706e37499d6661b34f
bnnlukas <bonn_lukas@gmx.de>
2022-02-06 17:18:59 +0100

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -5,8 +5,10 @@ let test
 test = new Crypto_Price_Api();
 console.log(await test.getSpotPrice('LINK','EUR'));
 
+//test 2
 test = new Crypto_Price_Api();
 console.log(await test.getBuyPrice('LINK','EUR'));
 
+//test 3
 test = new Crypto_Price_Api();
 console.log(await test.getSellPrice('LINK','EUR'));
