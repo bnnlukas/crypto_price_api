c2be4fc43d9a7e8f4db848404e0cf0ad4d8a5970
bnnlukas <bonn_lukas@gmx.de>
2022-02-06 18:59:20 +0100

    Files updated

--- crypto_price_api.ts
+++ crypto_price_api.ts
@@ -1,7 +1,7 @@
 let fiat_currency
 let crypto_currency
 
-export class Crypto_Price_Api {
+export class CryptoPriceApi {
 
     async getSpotPrice(crypto_currency:String, fiat_currency:String) {
         let data = await fetch(`https://api.coinbase.com/v2/prices/${crypto_currency}-${fiat_currency}/spot`);
--- tests/test.ts
+++ tests/test.ts
@@ -1,14 +1,14 @@
-import { Crypto_Price_Api } from '../crypto_price_api.ts'
+import { CryptoPriceApi } from '../crypto_price_api.ts'
 let test 
 
 //test 1
-test = new Crypto_Price_Api();
+test = new CryptoPriceApi();
 console.log(await test.getSpotPrice('LINK','EUR'));
 
 //test 2
-test = new Crypto_Price_Api();
+test = new CryptoPriceApi();
 console.log(await test.getBuyPrice('LINK','EUR'));
 
 //test 3
-test = new Crypto_Price_Api();
+test = new CryptoPriceApi();
 console.log(await test.getSellPrice('LINK','EUR'));
